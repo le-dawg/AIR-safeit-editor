03cfd9e009f7c9d0b58b2444e269a30a14606b76
dawg <dawid.golebiewski@tuhh.de>
2025-04-28 20:25:32 +0200

    attempting digirtalcoean support

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -2,12 +2,8 @@ console.log("DISABLE_ESLINT_PLUGIN:", process.env.DISABLE_ESLINT_PLUGIN);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
+  eslint: {
+    ignoreDuringBuilds: true,
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
